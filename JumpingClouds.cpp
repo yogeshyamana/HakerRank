52c0ebc0661011a776f6c59b3384ab50efac8c26
Yogesh Yadav <65649574+yogeshyamana@users.noreply.github.com>
2021-03-16 14:11:35 +0530

    Update JumpingClouds.cpp

--- JumpingClouds.cpp
+++ JumpingClouds.cpp
@@ -3,20 +3,15 @@ using namespace std;
 
 int solve(vector<int> c, int k){
     int ans=100;
-    int i=0;
-    while(i){
-        i = (i+k)%10;
-        if(c[i]==1){
-            ans = ans -1-2;
-            i +=k;
+    int start=0;
+    int n = c.size();
+    do{
+        ans--;
+        start = (start+k)%n;
+        if(c[start]==1){
+            ans = ans-2;
         }
-        else {
-            ans = ans-1;
-            i+=k;
-        }
-       
-    }
-   
+    }while(start != 0)
     return ans;
 }
 
